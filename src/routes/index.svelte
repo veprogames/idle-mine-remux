04fc983b118569764dfeb2800f6d9a25a50517e7
veprogames <veprogames.gamedev@gmail.com>
2022-07-06 20:38:12 +0200

    add roboto fonts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,6 @@
 <script>
     import game from "$lib/store/gamestore";
-    import Decimal from "break_infinity.js";
 </script>
 
-<h1 class="text-3xl font-bold underline">
-Hello world! {$game.test}
-<button class="uppercase font-semibold" on:click={() => $game.test = $game.test.add(1)}>++++++</button>
-</h1>
+<h1 class="text-3xl">You have <span class="font-mono">{$game.test}</span></h1>
+<button class="uppercase font-semibold" on:click={() => $game.test = $game.test.add(1)}>Get more</button>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,10 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      mono: ["Roboto Mono", "monospace"]
+    },
     extend: {},
   },
   plugins: [],
