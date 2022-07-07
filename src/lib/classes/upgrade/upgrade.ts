72e2a053518d63914789361e6fbc84330ed20379
veprogames <veprogames.gamedev@gmail.com>
2022-07-07 20:13:00 +0200

    buy upgrade method and new constructor

--- src/lib/classes/upgrade/upgrade.ts
+++ src/lib/classes/upgrade/upgrade.ts
@@ -1,8 +1,10 @@
 import Decimal from "break_infinity.js";
+import game from "$lib/store/gamestore";
+import { get } from "svelte/store";
 
 interface UpgradeDefinition{
-    name: String,
-    description: String,
+    name: string,
+    description: string,
     getPrice: ((level: number) => Decimal),
     getEffect: ((level: number) => Decimal),
     maxLevel: number
@@ -21,6 +23,7 @@ export class Upgrade{
     getEffect: ((level: number) => Decimal)
 
     level: number = 0
+    maxLevel: number = Infinity
     resource: UpgradeResource = UpgradeResource.NONE
 
     price: Decimal = new Decimal(0)
@@ -28,11 +31,12 @@ export class Upgrade{
     effect: Decimal = new Decimal(0)
     nextLevelEffect: Decimal = new Decimal(0)
 
-    constructor(name: string, description: string, getPrice: ((level: number) => Decimal), getEffect: ((level: number) => Decimal)){
+    constructor({name, description, getPrice, getEffect, maxLevel}: UpgradeDefinition){
         this.name = name;
         this.description = description;
         this.getPrice = getPrice;
         this.getEffect = getEffect;
+        this.maxLevel = maxLevel ?? Infinity;
     }
 
     /** Cache Prices and Effects into class members to save recalculations */
@@ -42,11 +46,38 @@ export class Upgrade{
         this.effect = this.getEffect(this.level);
         this.nextLevelEffect = this.getEffect(this.level + 1);
     }
+
+    private getResourceAmount(){
+        return get(game).money.amount;
+    }
+
+    private subPriceFromResource(){
+        return game.update(game => {
+            game.money.amount = game.money.amount.sub(this.price);
+            return game;
+        });
+    }
+
+    canAfford(){
+        return this.getResourceAmount().gte(this.price);
+    }
+
+    canBuy(){
+        return this.canAfford() && this.level < this.maxLevel;
+    }
+
+    buy(){
+        if(this.canBuy()){
+            this.subPriceFromResource();
+            this.level++;
+            this.update();
+        }
+    }
 }
 
-/*export class MoneyUpgrade extends Upgrade{
-    constructor(){
-        super("", "", () => null, () => null);
+export class MoneyUpgrade extends Upgrade{
+    constructor(config: UpgradeDefinition){
+        super(config);
         this.resource = UpgradeResource.MONEY
     }
-}*/
+}
