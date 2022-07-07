0534c8ca4397b679ea797caf5360f914a973ba82
veprogames <veprogames.gamedev@gmail.com>
2022-07-07 18:45:28 +0200

    some typescript

--- src/lib/classes/content/content-gems.ts
+++ src/lib/classes/content/content-gems.ts
@@ -1,9 +1,13 @@
 import Decimal from "break_infinity.js"
 
 export default class ContentGems{
+    amount: Decimal
+    highestGems: Decimal
+    upgrades: Object
+
     constructor(){
         this.amount = new Decimal(0);
-        this.highestMoney = new Decimal(0);
+        this.highestGems = new Decimal(0);
         this.upgrades = {};
     }
 }
--- src/lib/classes/content/content-money.ts
+++ src/lib/classes/content/content-money.ts
@@ -1,6 +1,10 @@
 import Decimal from "break_infinity.js"
 
 export default class ContentMoney{
+    amount: Decimal
+    highestMoney: Decimal
+    upgrades: Object
+
     constructor(){
         this.amount = new Decimal(0);
         this.highestMoney = new Decimal(0);
