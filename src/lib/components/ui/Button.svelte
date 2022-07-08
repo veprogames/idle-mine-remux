0b34cb35b745de4da8384189d410482e4bca1bc6
veprogames <veprogames.gamedev@gmail.com>
2022-07-08 10:51:44 +0200

    show upgrade level, full width button

--- src/lib/components/ui/Button.svelte
+++ src/lib/components/ui/Button.svelte
@@ -1,2 +1,7 @@
+<script>
+    export let full = false;
+</script>
+
 <button on:click on:dblclick on:mouseenter on:mouseleave
+class:w-full={full}
 class="p-2 rounded-lg font-semibold uppercase bg-blue-400 border-b-8 border-b-blue-800 hover:bg-blue-500 active:bg-blue-600"><slot/></button>
--- src/lib/components/upgrade/Upgrade.svelte
+++ src/lib/components/upgrade/Upgrade.svelte
@@ -1,30 +1,44 @@
 <script lang="ts">
-import type { Upgrade } from "$lib/classes/upgrade/upgrade";
-import Decimal from "break_infinity.js";
+    import type { Upgrade } from "$lib/classes/upgrade/upgrade";
+    import Decimal from "break_infinity.js";
 
-import Button from "../ui/Button.svelte";
+    import Button from "../ui/Button.svelte";
 
-export let upgrade: Upgrade|null = null;
-export let icon: string = "favicon.png";
+    export let upgrade: Upgrade|null = null;
+    export let icon: string = "favicon.png";
 
-$: price = upgrade?.price ?? new Decimal(0);
-
+    $: price = upgrade?.price ?? new Decimal(0);
 </script>
 
-<div on:click={() => upgrade?.buy()} class="group relative flex justify-between items-center p-2 w-72 rounded-md
-    bg-gradient-to-br from-slate-200 to-slate-400 dark:from-slate-600 dark:to-slate-800
+<div on:click={() => upgrade?.buy()} class="group relative flex justify-between items-center flex-wrap p-2 w-72 rounded-md
+    upgrade-sm-dark lg:upgrade-lg
     lg:w-16">
-    <img class="w-16 h-16" alt="UPG" src={icon}/>
+    <img class="w-14 h-14" alt="UPG" src={icon}/>
     <div class="lg:hidden flex flex-col flex-grow">
-        <p class="font-semibold uppercase text-lg"><slot name="title">Title</slot></p>
+        <p class="font-semibold uppercase text-lg"><slot name="title">Title</slot> Lv. {upgrade?.level}</p>
         <p><slot name="description">Description</slot></p>
     </div>
-    <div class="lg:hidden">
-        <Button on:click={() => upgrade?.buy()}><slot name="resource">$</slot> {price}</Button>
+    <div class="lg:hidden m-1 w-full">
+        <Button full={true} on:click={() => upgrade?.buy()}><slot name="resource">$</slot> {price}</Button>
     </div>
-    <div class="absolute top-full translate-y-4 scale-0 group-hover:lg:scale-100 bg-black rounded-md transition-all">
+    <span class="hidden lg:block absolute right-0 bottom-0 font-bold text-lg">{upgrade?.level}</span>
+    <div class="w-48 absolute top-full translate-y-4 scale-0 p-2 group-hover:lg:scale-100 bg-black rounded-md transition-all">
         <p class="font-semibold uppercase text-lg"><slot name="title">Title</slot></p>
         <p><slot name="description">Description</slot></p>
-        <p><slot name="resource">$</slot> {price}</p>
+        <p class="text-center text-lg"><slot name="resource">$</slot> <span class="font-mono">{price}</span></p>
     </div>
 </div>
+
+<style>
+    .upgrade-lg{
+        @apply cursor-default bg-blue-400 hover:bg-blue-500 active:bg-blue-600 border-b-4 border-b-blue-800;
+    }
+
+    .upgrade-sm{
+        @apply cursor-pointer bg-gradient-to-br from-slate-200 to-slate-400;
+    }
+
+    .upgrade-sm-dark{
+        @apply cursor-pointer bg-gradient-to-br from-slate-600 to-slate-800;
+    }
+</style>
