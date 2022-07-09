6d90ee0d86c5d71b6384652ea686db918baa7d61
veprogames <veprogames.gamedev@gmail.com>
2022-07-09 11:06:32 +0200

    fix upgrade design on window width

--- src/lib/components/upgrade/Upgrade.svelte
+++ src/lib/components/upgrade/Upgrade.svelte
@@ -7,38 +7,48 @@
     export let upgrade: Upgrade|null = null;
     export let icon: string = "favicon.png";
 
+    let screenWidth = 0;
+
     $: price = upgrade?.price ?? new Decimal(0);
+    $: isCompact = screenWidth > 1024;
 </script>
 
-<div on:click={() => upgrade?.buy()} class="group relative flex justify-between items-center flex-wrap p-2 w-72 rounded-md
-    upgrade-sm-dark lg:upgrade-lg
-    lg:w-16">
-    <img class="w-14 h-14" alt="UPG" src={icon}/>
-    <div class="lg:hidden flex flex-col flex-grow">
-        <p class="font-semibold uppercase text-lg"><slot name="title">Title</slot> Lv. {upgrade?.level}</p>
-        <p><slot name="description">Description</slot></p>
+<svelte:window bind:innerWidth={screenWidth}></svelte:window>
+
+{#if isCompact}
+    <div on:click={() => upgrade?.buy()} class="upgrade-base upgrade-lg group">
+        <img class="w-14 h-14" alt="UPG" src={icon}/>
+        <span class="absolute right-1 bottom-0 font-bold text-lg">{upgrade?.level}</span>
+        <!-- Tooltip -->
+        <div class="w-48 absolute top-full translate-y-4 scale-0 p-2 group-hover:scale-100 bg-black rounded-md transition-all">
+            <p class="font-semibold uppercase text-lg"><slot name="title">Title</slot></p>
+            <p><slot name="description">Description</slot></p>
+            <p class="text-center text-lg"><slot name="resource">$</slot> <span class="font-mono">{price}</span></p>
+        </div>
     </div>
-    <div class="lg:hidden m-1 w-full">
-        <Button full={true} on:click={() => upgrade?.buy()}><slot name="resource">$</slot> {price}</Button>
+{:else}
+    <div class="upgrade-base upgrade-sm group">
+        <img class="w-14 h-14" alt="UPG" src={icon}/>
+        <div class="flex flex-col flex-grow">
+            <p class="font-semibold uppercase text-lg"><slot name="title">Title</slot> Lv. {upgrade?.level}</p>
+            <p><slot name="description">Description</slot></p>
+        </div>
+        <div class="m-1 w-full">
+            <Button full={true} on:click={() => upgrade?.buy()}><slot name="resource">$</slot> {price}</Button>
+        </div>
     </div>
-    <span class="hidden lg:block absolute right-0 bottom-0 font-bold text-lg">{upgrade?.level}</span>
-    <div class="w-48 absolute top-full translate-y-4 scale-0 p-2 group-hover:lg:scale-100 bg-black rounded-md transition-all">
-        <p class="font-semibold uppercase text-lg"><slot name="title">Title</slot></p>
-        <p><slot name="description">Description</slot></p>
-        <p class="text-center text-lg"><slot name="resource">$</slot> <span class="font-mono">{price}</span></p>
-    </div>
-</div>
+{/if}
 
 <style>
+    .upgrade-base{
+        @apply relative flex justify-between items-center flex-wrap p-2 rounded-md;
+    }
+
     .upgrade-lg{
-        @apply cursor-default bg-blue-400 hover:bg-blue-500 active:bg-blue-600 border-b-4 border-b-blue-800;
+        @apply w-16 cursor-pointer bg-blue-400 hover:bg-blue-500 active:bg-blue-600 border-b-4 border-b-blue-800;
     }
 
     .upgrade-sm{
-        @apply cursor-pointer bg-gradient-to-br from-slate-200 to-slate-400;
-    }
-
-    .upgrade-sm-dark{
-        @apply cursor-pointer bg-gradient-to-br from-slate-600 to-slate-800;
+        @apply w-72 cursor-pointer bg-gradient-to-br from-slate-600 to-slate-800;
     }
 </style>
